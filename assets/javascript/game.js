84ced192ce79b5b999f2ce9e6eb3ba3e7aa0e02e
Claire Galya <clairegalya@Claires-MacBook-Pro.local>
2017-07-08 12:34:50 -0400

    seventh draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,6 @@ var modal = document.getElementById("myModal");
 window.onload = function() {
   document.addEventListener('keydown', newGame, false);
   modal.style.display = "block";
-  document.getElementById("start").innerHTML = "*Press any key to get started!*";
 }
 
 //removes previous event listener, inserts text, and starts the game
@@ -29,7 +28,7 @@ var start = function() {
   var word = words[Math.floor(Math.random() * words.length)];
 
   //used for creating the word guessing areas
-  var guesses = [];
+  var letterSpots = [];
   var wordSpot = document.getElementById('spot');
   var rightGuess = [];
 
@@ -48,12 +47,11 @@ var start = function() {
   var createWord = function() {
     word = words[Math.floor(Math.random() * words.length)];
     for (var i = 0; i < word.length; i++) {
-      var guess = document.createElement('div');
-      guess.setAttribute('id', 'guess');
-      guess.innerHTML = "_";
-      guesses.push(guess);
-      wordSpot.appendChild(guess);
-      
+      var underscore = document.createElement('div');
+      underscore.setAttribute('id', 'underscore');
+      underscore.innerHTML = "_";
+      letterSpots.push(underscore);
+      wordSpot.appendChild(underscore);
     }
     console.log(word);   
   }
@@ -65,7 +63,7 @@ var start = function() {
     var listLetters = document.createElement('div');
     for (var i = 0; i < word.length; i++) {
       if (word[i] === charTyped) {
-        guesses[i].innerHTML = charTyped;
+        letterSpots[i].innerHTML = charTyped;
         var yes = true;
         rightGuess.push(i);
       } 
@@ -86,37 +84,37 @@ var start = function() {
     /* if the word is guessed, a picture of the whale is displayed along with whale sounds*/
     if (rightGuess.length === word.length) {
       if (word === "narwhal") {
-        document.getElementById("img").innerHTML = "<img src='assets/images/narwhal.jpg'>";
+        document.getElementById("img").src = "assets/images/narwhal.jpg";
         var snd1 = new Audio("assets/sounds/whales1.wav");
         snd1.play();
       }
       if (word === "humpback") {
-        document.getElementById("img").innerHTML = "<img src='assets/images/humpback.jpg'>";
+        document.getElementById("img").src = "assets/images/humpback.jpg";
         var snd2 = new Audio("assets/sounds/whales2.wav");
         snd2.play();
       }
       if (word === "orca") {
-        document.getElementById("img").innerHTML = "<img src='assets/images/orca.jpg'>";
+        document.getElementById("img").src = "assets/images/orca.jpg";
         var snd3 = new Audio("assets/sounds/whales3.wav");
         snd3.play();
       }
       if (word === "blue") {
-        document.getElementById("img").innerHTML = "<img src='assets/images/blue.jpg'>";
+        document.getElementById("img").src = "assets/images/blue.jpg";
         var snd4 = new Audio("assets/sounds/whales4.wav");
         snd4.play();
       }
        if (word === "beluga") {
-        document.getElementById("img").innerHTML = "<img src='assets/images/beluga.jpg'>";
+        document.getElementById("img").src = "assets/images/beluga.jpg";;
         var snd5 = new Audio("assets/sounds/whales5.wav");
         snd5.play();
       }
        if (word === "bowhead") {
-        document.getElementById("img").innerHTML = "<img src='assets/images/bowhead.jpg'>";
+        document.getElementById("img").src = "assets/images/bowhead.jpg";
         var snd6 = new Audio("assets/sounds/whales6.wav");
         snd6.play();
       }
        if (word === "minke") {
-        document.getElementById("img").innerHTML = "<img src='assets/images/minke.jpg'>";
+        document.getElementById("img").src = "assets/images/minke.jpg";
         var snd7 = new Audio("assets/sounds/whales7.wav");
         snd7.play();
       }
@@ -134,7 +132,7 @@ var start = function() {
   var reset = function() {
     lives = 10
     showLives.innerHTML = "You have " + lives + " guesses remaining";
-    guesses = [];
+    letterSpots = [];
     rightGuess = [];
     document.getElementById("spot").innerHTML = "";
     document.getElementById("win").innerHTML = "";
